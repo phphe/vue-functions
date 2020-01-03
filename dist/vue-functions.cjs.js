9df7277ddbf1945752ac81f93626f270972a5299
phphe <phphe@outlook.com>
2020-01-03 20:34:32 +0800

    2.0.1-beta

--- dist/vue-functions.cjs.js
+++ dist/vue-functions.cjs.js
@@ -1,331 +1,14 @@
 /*!
-* vue-functions v2.0.0
-* (c) phphe <phphe@outlook.com> (https://github.com/phphe)
-* Released under the MIT License.
-*/
+ * vue-functions v2.0.1
+ * (c) phphe <phphe@outlook.com> (https://github.com/phphe)
+ * Released under the MIT License.
+ */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function _toConsumableArray(arr) {
-  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
-}
+var hp = require('helper-js');
 
-function _arrayWithoutHoles(arr) {
-  if (Array.isArray(arr)) {
-    for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
-
-    return arr2;
-  }
-}
-
-function _iterableToArray(iter) {
-  if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter);
-}
-
-function _nonIterableSpread() {
-  throw new TypeError("Invalid attempt to spread non-iterable instance");
-}
-
-var _marked =
-/*#__PURE__*/
-regeneratorRuntime.mark(iterateALL); // local store
-
-function isObject(v) {
-  return Object.prototype.toString.call(v) === '[object Object]';
-}
-
-function isFunction(v) {
-  return typeof v === 'function';
-}
-
-function isPromise(v) {
-  return Object.prototype.toString.call(v) === '[object Promise]';
-}
-
-
-function arrayRemove(arr, v) {
-  var index;
-  var count = 0;
-
-  while ((index = arr.indexOf(v)) > -1) {
-    arr.splice(index, 1);
-    count++;
-  }
-
-  return count;
-}
-
-
-function iterateALL(val) {
-  var opt,
-      i,
-      info,
-      _i7,
-      _Object$keys2,
-      key,
-      _info,
-      _i8,
-      _info2,
-      keys,
-      _i9,
-      _keys2,
-      _key2,
-      _info3,
-      _args = arguments;
-
-  return regeneratorRuntime.wrap(function iterateALL$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          opt = _args.length > 1 && _args[1] !== undefined ? _args[1] : {};
-
-          if (opt.reverse) {
-            _context.next = 30;
-            break;
-          }
-
-          if (!(val.length != null)) {
-            _context.next = 14;
-            break;
-          }
-
-          i = 0;
-
-        case 4:
-          if (!(i < val.length)) {
-            _context.next = 12;
-            break;
-          }
-
-          info = {
-            value: val[i],
-            index: i
-          };
-
-          if (!(!opt.exclude || !opt.exclude(info))) {
-            _context.next = 9;
-            break;
-          }
-
-          _context.next = 9;
-          return info;
-
-        case 9:
-          i++;
-          _context.next = 4;
-          break;
-
-        case 12:
-          _context.next = 28;
-          break;
-
-        case 14:
-          if (!isObject(val)) {
-            _context.next = 27;
-            break;
-          }
-
-          _i7 = 0, _Object$keys2 = Object.keys(val);
-
-        case 16:
-          if (!(_i7 < _Object$keys2.length)) {
-            _context.next = 25;
-            break;
-          }
-
-          key = _Object$keys2[_i7];
-          _info = {
-            value: val[key],
-            key: key
-          };
-
-          if (!(!opt.exclude || !opt.exclude(_info))) {
-            _context.next = 22;
-            break;
-          }
-
-          _context.next = 22;
-          return _info;
-
-        case 22:
-          _i7++;
-          _context.next = 16;
-          break;
-
-        case 25:
-          _context.next = 28;
-          break;
-
-        case 27:
-          throw 'Unsupported type';
-
-        case 28:
-          _context.next = 58;
-          break;
-
-        case 30:
-          if (!(val.length != null)) {
-            _context.next = 42;
-            break;
-          }
-
-          _i8 = val.length - 1;
-
-        case 32:
-          if (!(_i8 >= 0)) {
-            _context.next = 40;
-            break;
-          }
-
-          _info2 = {
-            value: val[_i8],
-            index: _i8
-          };
-
-          if (!(!opt.exclude || !opt.exclude(_info2))) {
-            _context.next = 37;
-            break;
-          }
-
-          _context.next = 37;
-          return _info2;
-
-        case 37:
-          _i8--;
-          _context.next = 32;
-          break;
-
-        case 40:
-          _context.next = 58;
-          break;
-
-        case 42:
-          if (!isObject(val)) {
-            _context.next = 57;
-            break;
-          }
-
-          keys = Object.keys(val);
-          keys.reverse();
-          _i9 = 0, _keys2 = keys;
-
-        case 46:
-          if (!(_i9 < _keys2.length)) {
-            _context.next = 55;
-            break;
-          }
-
-          _key2 = _keys2[_i9];
-          _info3 = {
-            value: val[_key2],
-            key: _key2
-          };
-
-          if (!(!opt.exclude || !opt.exclude(_info3))) {
-            _context.next = 52;
-            break;
-          }
-
-          _context.next = 52;
-          return _info3;
-
-        case 52:
-          _i9++;
-          _context.next = 46;
-          break;
-
-        case 55:
-          _context.next = 58;
-          break;
-
-        case 57:
-          throw 'Unsupported type';
-
-        case 58:
-        case "end":
-          return _context.stop();
-      }
-    }
-  }, _marked);
-} // source: http://stackoverflow.com/questions/8817394/javascript-get-deep-value-from-object-by-passing-path-to-it-as-string
-
-
-function joinFunctionsByNext(funcs) {
-  var next = function next() {};
-
-  var _iteratorNormalCompletion5 = true;
-  var _didIteratorError5 = false;
-  var _iteratorError5 = undefined;
-
-  try {
-    for (var _iterator5 = iterateALL(funcs, {
-      reverse: true
-    })[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
-      var func = _step5.value.value;
-      var currentNext = next;
-      next = wrapFuncWithNext(func, currentNext);
-    }
-  } catch (err) {
-    _didIteratorError5 = true;
-    _iteratorError5 = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion5 && _iterator5["return"] != null) {
-        _iterator5["return"]();
-      }
-    } finally {
-      if (_didIteratorError5) {
-        throw _iteratorError5;
-      }
-    }
-  }
-
-  return next;
-
-  function wrapFuncWithNext(func, next) {
-    return function () {
-      for (var _len5 = arguments.length, args = new Array(_len5), _key7 = 0; _key7 < _len5; _key7++) {
-        args[_key7] = arguments[_key7];
-      }
-
-      return func.apply(void 0, [next].concat(args));
-    };
-  }
-} // promise
-
-
-function onDOM(el, name, handler) {
-  for (var _len6 = arguments.length, args = new Array(_len6 > 3 ? _len6 - 3 : 0), _key8 = 3; _key8 < _len6; _key8++) {
-    args[_key8 - 3] = arguments[_key8];
-  }
-
-  if (el.addEventListener) {
-    // 所有主流浏览器，除了 IE 8 及更早 IE版本
-    el.addEventListener.apply(el, [name, handler].concat(args));
-  } else if (el.attachEvent) {
-    // IE 8 及更早 IE 版本
-    el.attachEvent.apply(el, ["on".concat(name), handler].concat(args));
-  }
-}
-
-function offDOM(el, name, handler) {
-  for (var _len7 = arguments.length, args = new Array(_len7 > 3 ? _len7 - 3 : 0), _key9 = 3; _key9 < _len7; _key9++) {
-    args[_key9 - 3] = arguments[_key9];
-  }
-
-  if (el.removeEventListener) {
-    // 所有主流浏览器，除了 IE 8 及更早 IE版本
-    el.removeEventListener.apply(el, [name, handler].concat(args));
-  } else if (el.detachEvent) {
-    // IE 8 及更早 IE 版本
-    el.detachEvent.apply(el, ["on".concat(name), handler].concat(args));
-  }
-}
-
-var _marked$1 =
-/*#__PURE__*/
-regeneratorRuntime.mark(iterateObjectWithoutDollarDash);
 /**
  * [updatablePropsEvenUnbound description]
  * @param  {[type]} props [object or getter]
@@ -337,7 +20,7 @@ regeneratorRuntime.mark(iterateObjectWithoutDollarDash);
  */
 
 function updatablePropsEvenUnbound(props) {
-  if (isFunction(props)) {
+  if (hp.isFunction(props)) {
     props = props();
   } else {
     // object
@@ -354,15 +37,13 @@ function updatablePropsEvenUnbound(props) {
     }
 
     if (!prop.$localSetter) {
-      prop.$localSetter = function (value) {
-        return value;
-      };
+      prop.$localSetter = value => value;
     } // make standardProp
 
 
     var standardProp = {};
     standardProps[name] = standardProp;
-    Object.keys(props[name]).forEach(function (key) {
+    Object.keys(props[name]).forEach(key => {
       if (key[0] !== '$') {
         standardProp[key] = prop[key];
       }
@@ -384,41 +65,41 @@ function updatablePropsEvenUnbound(props) {
       localValueOfUpdatableProps: {}
     };
 
-    for (var _i = 0, _Object$keys = Object.keys(props); _i < _Object$keys.length; _i++) {
-      var _name = _Object$keys[_i];
+    for (var _name of Object.keys(props)) {
       t.localValueOfUpdatableProps[_name] = this[_name];
     }
 
     return t;
   };
 
-  var _loop2 = function _loop2() {
-    var name = _Object$keys2[_i2];
-    var prop = props[name];
+  var _loop2 = function _loop2(_name2) {
+    var prop = props[_name2];
 
-    component.watch[name] = function (value) {
-      this.localValueOfUpdatableProps[name] = prop.$localSetter(value, this);
+    component.watch[_name2] = function (value) {
+      this.localValueOfUpdatableProps[_name2] = prop.$localSetter(value, this);
     };
 
     var localName = prop.$localName;
     component.computed[localName] = {
-      get: function get() {
-        return this.localValueOfUpdatableProps[name];
+      get() {
+        return this.localValueOfUpdatableProps[_name2];
       },
-      set: function set(value) {
-        if (name === 'value') {
+
+      set(value) {
+        if (_name2 === 'value') {
           this.$emit('input', value);
         } else {
-          this.$emit("update:".concat(name), value);
+          this.$emit("update:".concat(_name2), value);
         }
 
-        this.localValueOfUpdatableProps[name] = prop.$localSetter(value, this);
+        this.localValueOfUpdatableProps[_name2] = prop.$localSetter(value, this);
       }
+
     };
   };
 
-  for (var _i2 = 0, _Object$keys2 = Object.keys(props); _i2 < _Object$keys2.length; _i2++) {
-    _loop2();
+  for (var _name2 of Object.keys(props)) {
+    _loop2(_name2);
   }
 
   return component;
@@ -436,18 +117,14 @@ function watchAsync(vm, getter, handler, opt) {
   return destroy;
 
   function destroy() {
-    destroies.forEach(function (f) {
-      return f();
-    });
+    destroies.forEach(f => f());
     destroies = [];
   }
 
   function exec(getter, opt) {
     var value;
     var first = true;
-    var unwatch = vm.$watch(function () {
-      return getter.call(vm, exec);
-    }, function (value2) {
+    var unwatch = vm.$watch(() => getter.call(vm, exec), value2 => {
       value = value2;
 
       if (first) {
@@ -470,7 +147,7 @@ function watchAsync(vm, getter, handler, opt) {
     var localCount = count;
     oldValue = value;
 
-    var getterExecuted = function getterExecuted(value) {
+    var getterExecuted = value => {
       if (localCount !== count) {
         // expired
         return;
@@ -486,7 +163,7 @@ function watchAsync(vm, getter, handler, opt) {
     }; //
 
 
-    if (isPromise(result)) {
+    if (hp.isPromise(result)) {
       result.then(getterExecuted);
     } else {
       getterExecuted(result);
@@ -497,9 +174,9 @@ function watchAsync(vm, getter, handler, opt) {
 function doWatch(vm, handler) {
   var oldValue, unwatch;
 
-  var update = function update() {
+  var update = () => {
     var getter = handler.call(vm, oldValue);
-    unwatch = vm.$watch(getter, function (value) {
+    unwatch = vm.$watch(getter, value => {
       unwatch();
       oldValue = value;
       update();
@@ -507,52 +184,23 @@ function doWatch(vm, handler) {
   };
 
   update();
-  return function () {
-    return unwatch && unwatch();
-  };
+  return () => unwatch && unwatch();
 }
-function iterateObjectWithoutDollarDash(obj) {
-  var key, start;
-  return regeneratorRuntime.wrap(function iterateObjectWithoutDollarDash$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          _context.t0 = regeneratorRuntime.keys(obj);
+function* iterateObjectWithoutDollarDash(obj) {
+  for (var key in obj) {
+    var start = key.substr(0, 1);
 
-        case 1:
-          if ((_context.t1 = _context.t0()).done) {
-            _context.next = 9;
-            break;
-          }
-
-          key = _context.t1.value;
-          start = key.substr(0, 1);
-
-          if (!(start !== '$' && start !== '_')) {
-            _context.next = 7;
-            break;
-          }
-
-          _context.next = 7;
-          return {
-            key: key,
-            value: obj[key]
-          };
-
-        case 7:
-          _context.next = 1;
-          break;
-
-        case 9:
-        case "end":
-          return _context.stop();
-      }
+    if (start !== '$' && start !== '_') {
+      yield {
+        key,
+        value: obj[key]
+      };
     }
-  }, _marked$1);
+  }
 } // add reactive `windowSize`
 
 var windowSize = {
-  data: function data() {
+  data() {
     return {
       windowSize: {
         innerWidth: window.innerWidth,
@@ -562,8 +210,9 @@ var windowSize = {
       }
     };
   },
+
   methods: {
-    updateWindowSize: function updateWindowSize() {
+    updateWindowSize() {
       Object.assign(this.windowSize, {
         innerWidth: window.innerWidth,
         innerHeight: window.innerHeight,
@@ -571,26 +220,27 @@ var windowSize = {
         outerHeight: window.outerHeight
       });
     }
+
   },
-  created: function created() {
-    var _this = this;
 
-    this._windowSize_onresize = function () {
-      _this.updateWindowSize();
-
-      _this.$emit('window-resize');
+  created() {
+    this._windowSize_onresize = () => {
+      this.updateWindowSize();
+      this.$emit('window-resize');
     };
 
-    onDOM(window, 'resize', this._windowSize_onresize);
+    hp.onDOM(window, 'resize', this._windowSize_onresize);
   },
-  beforeDestroy: function beforeDestroy() {
-    offDOM(window, 'resize', this._windowSize_onresize);
+
+  beforeDestroy() {
+    hp.offDOM(window, 'resize', this._windowSize_onresize);
   }
+
 };
 function registerPreventURLChange(Vue, router, msg) {
   var preventRouter = false;
   var msg0 = "It looks like you have been editing something.\nIf you leave before saving, your changes will be lost.";
-  router.beforeEach(function (to, from, next) {
+  router.beforeEach((to, from, next) => {
     if (preventRouter) {
       if (window.confirm(msg || msg0)) {
         Vue.allowURLChange();
@@ -603,14 +253,14 @@ function registerPreventURLChange(Vue, router, msg) {
     }
   });
 
-  var beforeunload = function beforeunload(e) {
+  var beforeunload = e => {
     var confirmationMessage = msg || msg0;
     e.returnValue = confirmationMessage; // Gecko, Trident, Chrome 34+
 
     return confirmationMessage; // Gecko, WebKit, Chrome <34
   };
 
-  Vue.preventURLChange = Vue.prototype.$preventURLChange = function (msg2) {
+  Vue.preventURLChange = Vue.prototype.$preventURLChange = msg2 => {
     if (msg2 != null) {
       msg = msg2;
     }
@@ -621,7 +271,7 @@ function registerPreventURLChange(Vue, router, msg) {
     }
   };
 
-  Vue.allowURLChange = Vue.prototype.$allowURLChange = function () {
+  Vue.allowURLChange = Vue.prototype.$allowURLChange = () => {
     preventRouter = false;
     window.removeEventListener("beforeunload", beforeunload);
   };
@@ -630,12 +280,13 @@ var hookHelper = {
   methods: {
     // todo extract hooks to vue-functions
     // get hooks in this._hooks, without which in props
-    _getNonPropHooksByName: function _getNonPropHooksByName(name) {
+    _getNonPropHooksByName(name) {
       if (this._hooks) {
         return this._hooks[name];
       }
     },
-    addHook: function addHook(name, func) {
+
+    addHook(name, func) {
       if (!this._getNonPropHooksByName(name)) {
         if (!this._hooks) {
           this._hooks = {};
@@ -648,33 +299,37 @@ var hookHelper = {
 
       this._hooks[name].push(func);
     },
-    removeHook: function removeHook(name, func) {
+
+    removeHook(name, func) {
       var hooks = this._getNonPropHooksByName(name);
 
       if (hooks) {
-        arrayRemove(hooks, func);
+        hp.arrayRemove(hooks, func);
       }
     },
-    hasHook: function hasHook(name) {
+
+    hasHook(name) {
       return this._getNonPropHooksByName(name) || this[name];
     },
-    executeHook: function executeHook(name, args) {
+
+    executeHook(name, args) {
       var hooks = this._getNonPropHooksByName(name).slice();
 
       if (hooks) {
-        if (this[name] && isFunction(this[name])) {
+        if (this[name] && hp.isFunction(this[name])) {
           hooks.push(function (next) {
             for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
               args[_key - 1] = arguments[_key];
             }
 
-            return this[name].apply(this, args);
+            return this[name](...args);
           });
         }
 
-        return joinFunctionsByNext(hooks).apply(void 0, _toConsumableArray(args));
+        return hp.joinFunctionsByNext(hooks)(...args);
       }
     }
+
   }
 };
 
--- scripts/build.js
+++ scripts/build.js
@@ -1,257 +1,85 @@
-const gzipSize = require('gzip-size');
-const rollup = require('rollup');
-const JsonPlugin = require('@rollup/plugin-json');
-const NodeResolvePlugin = require('@rollup/plugin-node-resolve');
-const CommonjsPlugin = require('@rollup/plugin-commonjs');
-const BabelPlugin = require('rollup-plugin-babel');
-const TerserPlugin = require('rollup-plugin-terser'); // minify file
-const path = require('path');
-const fs = require('fs');
-const package = require('../package.json');
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const path = require('path')
+const zlib = require('zlib')
+const rollup = require('rollup')
 
-const program = require('commander');
-program.option('-w, --watch', 'Watch files change and build cjs, umd, esm')
-program.parse(process.argv);
-
-// The default entry is src/index.js or src/{package.name}.js
-if (program.watch) {
-  // watch
-  watchFile()
-} else {
-  // build
-  buildFile()
+if (!fs.existsSync('dist')) {
+  fs.mkdirSync('dist')
 }
 
-// No subdirectories; 不包含子目录
-async function buildDir(inputDir, opt={}, eachOpt={}) {
-  const files = fs.readdirSync(inputDir)
-  .filter(item => {
-    const filePath = path.join(inputDir, item)
-    return fs.statSync(filePath).isFile() && (!opt.exclude || !opt.exclude(item, filePath))
+let builds = require('./config').getAllBuilds()
+
+// filter builds via command line arg
+if (process.argv[2]) {
+  const filters = process.argv[2].split(',')
+  builds = builds.filter(b => {
+    return filters.some(f => b.output.file.indexOf(f) > -1 || b._name.indexOf(f) > -1)
   })
-  const report = []
-  for (const item of files) {
-    const filePath = path.join(inputDir, item)
-    const [name, suffix] = item.split('.')
-    const pathWithoutSuffix = item.slice(0, item.length - 1 - suffix.length)
-    report.push(...await buildFile({
-      input: filePath,
-      moduleName: camelCase(name),
-      outFileName: pathWithoutSuffix,
-      ...eachOpt,
-    }))
+}
+
+build(builds)
+
+function build (builds) {
+  let built = 0
+  const total = builds.length
+  const next = () => {
+    buildEntry(builds[built]).then(() => {
+      built++
+      if (built < total) {
+        next()
+      }
+    }).catch(logError)
   }
+
+  next()
 }
-// No subdirectories; 不包含子目录
-function watchDir(inputDir, opt={}, eachOpt={}) {
-  const files = fs.readdirSync(inputDir)
-  .filter(item => {
-    const filePath = path.join(inputDir, item)
-    return fs.statSync(filePath).isFile() && (!opt.exclude || !opt.exclude(item, filePath))
-  })
-  for (const item of files) {
-    const filePath = path.join(inputDir, item)
-    const [name, suffix] = item.split('.')
-    const pathWithoutSuffix = item.slice(0, item.length - 1 - suffix.length)
-    watchFile({
-      input: filePath,
-      moduleName: camelCase(name),
-      outFileName: pathWithoutSuffix,
-      ...eachOpt,
+
+function buildEntry (config) {
+  const output = config.output
+  const isZip = Boolean(config.plugins.find(v => v.name === 'terser'))
+  const { file, banner } = output
+  return rollup.rollup(config)
+    .then(bundle => bundle.generate(output))
+    .then(async bundle => {
+      const {code, map} = bundle.output[0]
+      if (output.sourcemap) {
+        const mapPath = `${file}.map`
+        await write(mapPath, map.toString())
+      }
+      await write(file, code, isZip)
     })
-  }
-}
-/*
-opt.plugins: Your plugin config will override default by name, or be pushed to end of plugins.
-hooks:
-  opt.afterOptionsResolved(inputOptions, outputOptions)
- */
-async function buildFile(opt={}) {
-  opt = {
-    input: get_default_input(),
-    moduleName: camelCase(package.name),
-    outFileName: package.name, // without suffix
-    outputDir: './dist',
-    formats: ['cjs', 'esm', 'umd', 'umd.min'],
-    ...opt,
-    plugins: resolvePlugins(opt.plugins),
-  }
-  //
-  const report = []
-  for (const format of opt.formats) {
-    const inputOptions = {
-      input: opt.input,
-      plugins: [...opt.plugins],
-    }
-    const outputOptions = {
-      format: format,
-      name: opt.moduleName,
-      exports: 'named', // reason: Entry module "xxx.js" is using named and default exports together. Consumers of your bundle will have to use `helperJs["default"]` to access the default export, which may not be what you want. Use `output.exports: "named"` to disable this warning
-    }
-    if (opt.banner !== false) {
-      if (typeof opt.banner === 'string') {
-        outputOptions.banner = opt.banner
-      } else {
-        outputOptions.banner = `
-/*!
-* ${package.name} v${package.version}
-* (c) ${package.author}
-* Released under the ${package.license} License.
-*/`.trim()
-      }
-    }
-    switch (format) {
-      case 'umd':
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.js`)
-        break;
-      case 'umd.min':
-        inputOptions.plugins.push(TerserPlugin.terser())
-        outputOptions.format = 'umd'
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.min.js`)
-        outputOptions.sourcemap = true
-        break;
-      default:
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.${format}.js`)
-    }
-    opt.afterOptionsResolved && opt.afterOptionsResolved(inputOptions, outputOptions)
-    const bundle = await rollup.rollup(inputOptions)
-    // generate code
-    await bundle.write(outputOptions)
-    reportOne(outputOptions.file)
-    if (format === 'umd.min') {
-      const sourceMapPath = outputOptions.file + '.map'
-      if (fs.existsSync(sourceMapPath)) {
-        reportOne(sourceMapPath)
-      }
-    }
-  }
-  console.table(report);
-  return report
-  function reportOne(file) {
-    const {sizeKiB, sizeKiBGzipped} = getFileInfo(file)
-    console.log(`Done: ${file}`);
-    report.push({Output: file, Size: `${sizeKiB} KiB`, Gzipped: `${sizeKiBGzipped} KiB`})
-  }
 }
 
-/*
-opt.plugins: Your plugin config will override default by name, or be pushed to end of plugins.
-hooks:
-  opt.afterOptionsResolved(watchOptions)
- */
-async function watchFile(opt={}) {
-  opt = {
-    input: get_default_input(),
-    moduleName: camelCase(package.name),
-    outFileName: package.name, // without suffix
-    outputDir: './dist',
-    formats: ['cjs', 'esm', 'umd'],
-    plugins: resolvePlugins(opt.plugins),
-    ...opt,
-  }
-  const inputOptions = {
-    input: opt.input,
-    plugins: [...opt.plugins],
-  }
-  const outputOptionsArr = []
-  //
-  for (const format of opt.formats) {
-    const outputOptions = {
-      format: format,
-      name: opt.moduleName,
-      exports: 'named', // reason: Entry module "xxx.js" is using named and default exports together. Consumers of your bundle will have to use `helperJs["default"]` to access the default export, which may not be what you want. Use `output.exports: "named"` to disable this warning
+function write (dest, code, zip) {
+  return new Promise((resolve, reject) => {
+    function report (extra) {
+      console.log(blue(path.relative(process.cwd(), dest)) + ' ' + getSize(code) + (extra || ''))
+      resolve()
     }
-    outputOptionsArr.push(outputOptions)
-    switch (format) {
-      case 'umd':
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.js`)
-        break;
-      case 'umd.min':
-        inputOptions.plugins.push(TerserPlugin.terser())
-        outputOptions.format = 'umd'
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.min.js`)
-        outputOptions.sourcemap = true
-        break;
-      default:
-        outputOptions.file = path.join(opt.outputDir, `${opt.outFileName}.${format}.js`)
-    }
-  }
-  const watchOptions = {
-    ...inputOptions,
-    output: outputOptionsArr,
-    // watch: {
-    //   chokidar,
-    //   clearScreen,
-    //   exclude,
-    //   include
-    // }
-  };
-  opt.afterOptionsResolved && opt.afterOptionsResolved(watchOptions)
-  console.log(`Start to watch ${inputOptions.input}`);
-  const watcher = rollup.watch(watchOptions);
-  watcher.on('event', event => {
-    if (['ERROR', 'FATAL'].includes(event.code)) {
-      console.log('error', '======================================================');
-      console.log(event.error);
-    }
-    if (['BUNDLE_END'].includes(event.code)) {
-      console.log('success', '======================================================');
-      console.log(event);
-    }
-    // event.code can be one of:
-    //   START        — the watcher is (re)starting
-    //   BUNDLE_START — building an individual bundle
-    //   BUNDLE_END   — finished building a bundle
-    //   END          — finished building all bundles
-    //   ERROR        — encountered an error while bundling
-    //   FATAL        — encountered an unrecoverable error
-  });
-}
-
-function get_default_input() {
-  let input = './src/index.js'
-  if (!fs.existsSync(input)) {
-    input = `./src/${package.name.js}`
-  }
-  return input
-}
-function resolvePlugins(inputPlugins) {
-  const defaultPlugins = [
-    BabelPlugin({
-      runtimeHelpers: true,
-    }),
-    NodeResolvePlugin(),
-    CommonjsPlugin(),
-    JsonPlugin(),
-  ];
-  if (inputPlugins) {
-    inputPlugins.forEach(plugin => {
-      const index = defaultPlugins.findIndex(v => v.name === plugin.name)
-      if (index > -1) {
-        // replace with input plugin
-        defaultPlugins.splice(index, 1, plugin)
+    mkdirp.sync(path.dirname(dest))
+    fs.writeFile(dest, code, err => {
+      if (err) return reject(err)
+      if (zip) {
+        zlib.gzip(code, (err, zipped) => {
+          if (err) return reject(err)
+          report(' (gzipped: ' + getSize(zipped) + ')')
+        })
       } else {
-        defaultPlugins.push(plugin)
+        report()
       }
     })
-  }
-  return defaultPlugins
+  })
 }
-function studlyCase (str) {
-  return str && (str[0].toUpperCase() + str.substr(1))
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
 }
-function camelCase (str) {
-  const temp = str.toString().split(/[-_]/)
-  for (let i = 1; i < temp.length; i++) {
-    temp[i] = studlyCase(temp[i])
-  }
-  return temp.join('')
+
+function logError (e) {
+  console.log(e)
 }
-function getFileInfo(file) {
-  // get size
-  const stats = fs.statSync(file)
-  const sizeKiB = parseFloat((stats["size"] / 1024).toFixed(2))
-  // get gzipped size
-  const sizeKiBGzipped = parseFloat((gzipSize.fileSync(file) / 1024).toFixed(2))
-  return {file, sizeKiB, sizeKiBGzipped}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
 }
